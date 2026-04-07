e174e1302ecf1d4dcc34a2cacf900a01f1a2567b
daniel <daniel.blindr@gmail.com>
2026-04-07 21:31:20 +0100

    Splash screen manager

---
+++ scenes/ui/splash_screen/splash_screen.gd
@@ -0,0 +1,95 @@
+extends Control
+## Scene for displaying opening logos, placards, or other images before a game.
+
+## The list of images to show in the opening sequence.
+@export var images: Array[Texture2D]
+@export_group("Animation")
+## The time to fade-in the next image.
+@export var fade_in_time: float = 0.2
+## The time to fade-out the previous image.
+@export var fade_out_time: float = 0.2
+## The time to keep an image visible after fade-in and before fade-out.
+@export var visible_time: float = 1.6
+@export_group("Transition")
+## The delay before starting the first fade-in animation once ready.
+@export var start_delay: float = 0.5
+## The delay after ending the last fade-in animation before loading the next scene.
+@export var end_delay: float = 0.5
+## If true, show a loading screen if the next scene is not yet ready.
+@export var show_loading_screen: bool = false
+
+var _tween: Tween
+var _next_image_index: int = 0
+
+@onready var _container: MarginContainer = $MarginContainer
+
+
+func _ready() -> void:
+	#SceneLoader.load_scene(get_next_scene_path(), true)
+	_add_textures_to_container(images)
+	_transition_in()
+
+
+func _add_textures_to_container(textures: Array[Texture2D]) -> void:
+	for texture in textures:
+		var texture_rect: TextureRect = TextureRect.new()
+		texture_rect.texture = texture
+		texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		texture_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		texture_rect.modulate.a = 0.0
+		_container.call_deferred("add_child", texture_rect)
+
+
+func _transition_in() -> void:
+	await get_tree().create_timer(start_delay).timeout
+	if _next_image_index == 0:
+		_show_next_image()
+
+
+func _transition_out() -> void:
+	await get_tree().create_timer(end_delay).timeout
+	_load_next_scene()
+
+
+func _show_next_image(animated: bool = true) -> void:
+	_hide_previous_image()
+	if _next_image_index >= images.size():
+		if animated:
+			_transition_out()
+		else:
+			_load_next_scene()
+		return
+	var texture_rect := _container.get_child(_next_image_index) as TextureRect
+	if animated:
+		_tween = create_tween()
+		_tween.tween_property(texture_rect, "modulate:a", 1.0, fade_in_time)
+		await _tween.finished
+	else:
+		texture_rect.modulate.a = 1.0
+	_next_image_index += 1
+	_wait_and_fade_out(texture_rect)
+
+
+func _hide_previous_image() -> void:
+	if _tween and _tween.is_running():
+		_tween.stop()
+	if images.size() == 0:
+		return
+	var current_image := _container.get_child(_next_image_index - 1) as TextureRect
+	if current_image:
+		current_image.modulate.a = 0.0
+
+
+func _wait_and_fade_out(texture_rect: TextureRect) -> void:
+	var compare_next_index := _next_image_index
+	await get_tree().create_timer(visible_time).timeout
+	if compare_next_index != _next_image_index:
+		return
+	_tween = create_tween()
+	_tween.tween_property(texture_rect, "modulate:a", 0.0, fade_out_time)
+	await _tween.finished
+	_show_next_image.call_deferred()
+
+
+func _load_next_scene() -> void:
+	print("Loading next scene")
