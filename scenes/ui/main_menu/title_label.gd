f1767696d91b2b432d40da184f4ddf9825252855
daniel <daniel.blindr@gmail.com>
2026-04-09 18:54:22 +0100

    Main menu scene

---
+++ scenes/ui/main_menu/title_label.gd
@@ -0,0 +1,19 @@
+@tool
+extends Label
+
+const DEFAULT_TITLE = "Title"
+
+## If true, the label will automatically update its text based on project settings.
+@export var auto_update := true
+
+
+func _ready() -> void:
+	if auto_update:
+		_update_label()
+
+
+func _update_label() -> void:
+	var config_name: String = ProjectSettings.get_setting("application/config/name", DEFAULT_TITLE)
+	if config_name.is_empty():
+		config_name = DEFAULT_TITLE
+	text = config_name
